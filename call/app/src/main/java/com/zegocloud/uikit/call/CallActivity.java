568843e63aad57c92879abec296b46953da7d3d6
alan <alanliu@zego.im>
2022-09-22 17:20:57 +0800

    update files

--- call/app/src/main/java/com/zegocloud/uikit/call/CallActivity.java
+++ call/app/src/main/java/com/zegocloud/uikit/call/CallActivity.java
@@ -1,9 +1,11 @@
 package com.zegocloud.uikit.call;
 
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallConfig;
 import com.zegocloud.uikit.prebuilt.call.ZegoUIKitPrebuiltCallFragment;
+import java.util.Random;
 
 public class CallActivity extends AppCompatActivity {
 
@@ -19,13 +21,26 @@ public class CallActivity extends AppCompatActivity {
         long appID = ;
         String appSign = "";
 
-        String callID = callID;   // for example, "123456"
-        String userID = userID;// for example, Build.MANUFACTURER + "_" + generateUserID();
-        String userName = userName; // for example,  userID + "_Name";
+        String callID = getIntent().getStringExtra("callID");
+        String userID = Build.MANUFACTURER + "_" + generateUserID();
+        String userName = userID + "_Name";
 
         ZegoUIKitPrebuiltCallConfig config = ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall();
         ZegoUIKitPrebuiltCallFragment fragment = ZegoUIKitPrebuiltCallFragment.newInstance(appID, appSign, userID,
             userName, callID, config);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commitNow();
+    }
+
+    private String generateUserID() {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        while (builder.length() < 5) {
+            int nextInt = random.nextInt(10);
+            if (builder.length() == 0 && nextInt == 0) {
+                continue;
+            }
+            builder.append(nextInt);
+        }
+        return builder.toString();
     }
 }
--- call/app/src/main/java/com/zegocloud/uikit/call/MainActivity.java
+++ call/app/src/main/java/com/zegocloud/uikit/call/MainActivity.java
@@ -1,8 +1,9 @@
 package com.zegocloud.uikit.call;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.start_call).setOnClickListener(v -> {
+        TextInputLayout textInputLayout = findViewById(R.id.text_input_layout);
+        findViewById(R.id.join_btn).setOnClickListener(v -> {
+            String callID = textInputLayout.getEditText().getText().toString();
+            if (callID.isEmpty()) {
+                textInputLayout.setError("please input conferenceID");
+                return;
+            }
+            textInputLayout.setError("");
             Intent intent = new Intent(MainActivity.this, CallActivity.class);
+            intent.putExtra("callID", callID);
             startActivity(intent);
         });
+
+        textInputLayout.getEditText().setText("call_id");
     }
 }

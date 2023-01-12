93e3716877608cd6731b88ec01976f0e28d6c735
noonqiu <noonqiu@zego.im>
2023-01-12 21:06:47 +0800

    update to offline call

---
+++ call_with_offline_invitation/README.md
@@ -0,0 +1,127 @@
+# Quick start
+
+- - -
+
+<img src="https://storage.zego.im/sdk-doc/Pics/ZegoUIKit/Flutter/call/invitation_calls.gif" >
+
+
+## Prerequisites
+
+- Go to [ZEGOCLOUD Admin Console](https://console.zegocloud.com), and do the following:
+  - Create a project, get the **AppID** and **AppSign**.
+  - Activate the **In-app Chat** service (as shown in the following figure).
+
+![ActivateZIMinConsole](https://storage.zego.im/sdk-doc/Pics/InappChat/ActivateZIMinConsole2.png)
+
+
+## Add ZegoUIKitPrebuiltCall as dependencies
+
+1. Add the `jitpack` configuration.
+
+- If your Android Gradle Plugin is **7.1.0 or later**: enter your project's root directory, open the `settings.gradle` file to add the jitpack to `dependencyResolutionManagement` > `repositories` like this:
+
+``` groovy
+dependencyResolutionManagement {
+   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+   repositories {
+      google()
+      mavenCentral()
+      maven { url 'https://www.jitpack.io' } // <- Add this line.
+   }
+}
+```
+
+<div class="mk-warning">
+
+If you can't find the above fields in `settings.gradle`, it's probably because your Android Gradle Plugin version is lower than v7.1.0. 
+
+For more details, see [Android Gradle Plugin Release Note v7.1.0](https://developer.android.com/studio/releases/gradle-plugin#settings-gradle).
+</div>
+
+- If your Android Gradle Plugin is **earlier than 7.1.0**: enter your project's root directory, open the `build.gradle` file to add the jitpack to `allprojects`->`repositories` like this: 
+
+```groovy
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url "https://jitpack.io" }  // <- Add this line.
+    }
+}
+``` 
+
+2. Modify your app-level `build.gradle` file:
+```groovy
+dependencies {
+    ...
+    implementation 'com.github.ZEGOCLOUD:zego_uikit_prebuilt_call_android:1.4.0'    // Add this line to your module-level build.gradle file's dependencies, usually named [app].
+    implementation 'com.github.ZEGOCLOUD:zego_uikit_signaling_plugin_android:1.4.0'  // Add this line to your module-level build.gradle file's dependencies, usually named [app].
+    implementation platform('com.google.firebase:firebase-bom:29.3.1') // Add this line to your module-level build.gradle file's dependencies，Import the Firebase BoM。
+}
+```  
+3. follow the instructions in the video below.
+
+<video poster="http://storage.zego.im/Pics/ZegoUIKit/videos/how_to_enable_offline_call_invitation_android.png" src="https://storage.zego.im/sdk-doc/doc/video/ZegoUIKit/callkit_zpns_config1.mov" width="80%" preload="auto" controls></video>
+
+Resource may help: [Firebase Console](https://console.firebase.google.com/)
+
+
+## Integrate the SDK with the call invitation feature
+
+### 1、Initialize the call invitation service
+
+Call the `init` method on the App startup, and specify the `userID` and `userName` for connecting the Call Kit service. 
+
+- Go to [ZEGOCLOUD Admin Console](https://console.zegocloud.com/), get the `appID` and `appSign` of your project.
+- `userID` can be something like a phone number or the user ID on your own user system. userID can only contain numbers, letters, and underlines (_).
+- `userName` can be any character or the user name on your own user system.
+
+### 2、Add the button for making call invitations
+
+You can customize the position of the `ZegoSendCallInvitationButton` accordingly, pass in the ID of the user you want to call.
+
+```java
+public class MainActivity extends AppCompatActivity {
+    long appID = YourAppID;
+    String appSign = YourAppSign;
+    String userID = "userID";
+    String userName = "userName";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initSendCallInvitationButton();
+        initCallInviteService();
+    }
+
+    public void initCallInviteService() {
+      ZegoUIKitPrebuiltCallInvitationConfig callInvitationConfig = new ZegoUIKitPrebuiltCallInvitationConfig(ZegoSignalingPlugin.getInstance());
+      //Change notifyWhenAppRunningInBackgroundOrQuit to false if you don't need to receive a call invitation notification while your app running in the background or quit.
+      callInvitationConfig.notifyWhenAppRunningInBackgroundOrQuit = true;
+      //This property needs to be set when you are building an Android app and when the notifyWhenAppRunningInBackgroundOrQuit is true. notificationConfig.channelID must be the same as the FCM Channel ID in ZEGOCLOUD Admin Console, and the notificationConfig.channelName can be an arbitrary value. The notificationConfig.soundmust be the same as the FCM sound in Admin Console either.
+      ZegoNotificationConfig notificationConfig = new ZegoNotificationConfig();
+      notificationConfig.sound = "zego_uikit_sound_call";
+      notificationConfig.channelID = "CallInvitation";
+      notificationConfig.channelName = "CallInvitation";
+      ZegoUIKitPrebuiltCallInvitationService.init(getApplication(), appID, appSign, userID, userName,callInvitationConfig);
+    }
+
+    private void initSendCallInvitationButton(){
+        String targetUserID = ; // The ID of the user you want to call.
+        Context context = ; // Android context.
+
+        ZegoSendCallInvitationButton button = new ZegoSendCallInvitationButton(context);
+        //	If true, a video call is made when the button is pressed. Otherwise, a voice call is made.
+        button.setIsVideoCall(true);
+        //resourceID can be used to specify the ringtone of an offline call invitation, which must be set to the same value as the Push Resource ID in ZEGOCLOUD Admin Console. This only takes effect when the notifyWhenAppRunningInBackgroundOrQuit is true.
+        button.setResourceID("zego_uikit_call");
+        button.setInvitees(Collections.singletonList(new ZegoUIKitUser(targetUserID)));
+    }
+}
+```
+
+Then, you can create a call invitation by starting your `MainActivity`.
+
+## Related guide
+[Custom Prebuilt UI](https://docs.zegocloud.com/article/14766)
---
+++ call_with_offline_invitation/app/src/main/java/com/zegocloud/uikit/callwithofflineinvitation/MainActivity.java
@@ -0,0 +1,199 @@
+package com.zegocloud.uikit.callwithofflineinvitation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.zegocloud.uikit.plugin.signaling.ZegoSignalingPlugin;
+import com.zegocloud.uikit.prebuilt.call.config.ZegoNotificationConfig;
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationConfig;
+import com.zegocloud.uikit.prebuilt.call.invite.ZegoUIKitPrebuiltCallInvitationService;
+import com.zegocloud.uikit.prebuilt.call.invite.internal.ClickListener;
+import com.zegocloud.uikit.prebuilt.call.invite.internal.ZegoCallUser;
+import com.zegocloud.uikit.prebuilt.call.invite.widget.ZegoSendCallInvitationButton;
+import com.zegocloud.uikit.service.defines.ZegoUIKitUser;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String userName;
+    private SharedPreferences sp;
+
+    private ZegoUIKitPrebuiltCallInvitationConfig callInvitationConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("offline", Context.MODE_PRIVATE);
+
+        TextView yourUserID = findViewById(R.id.your_user_id);
+        String userID = getUserId();
+
+        yourUserID.setText("Your User ID :" + userID);
+
+        initCallInviteService(userID);
+
+        initVoiceButton();
+
+        initVideoButton();
+
+        Button button = findViewById(R.id.show_munu);
+        button.setText(getButtonStatus() ? "显示拒绝按钮" : "隐藏拒绝按钮");
+
+        button.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(this, button);
+            popup.getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
+            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    int itemId = item.getItemId();
+                    if (itemId == R.id.option_1) {
+                        callInvitationConfig.showDeclineButton = true;
+                        saveButtonStatus(true);
+                    } else if (itemId == R.id.option_2) {
+                        callInvitationConfig.showDeclineButton = false;
+                        saveButtonStatus(false);
+                    }
+                    button.setText(item.getTitle());
+                    return true;
+                }
+            });
+            popup.setOnDismissListener(menu -> {
+
+            });
+            popup.show();
+        });
+    }
+
+
+    private void initVideoButton() {
+        ZegoSendCallInvitationButton newVideoCall = findViewById(R.id.new_video_call);
+        newVideoCall.setIsVideoCall(true);
+        //resourceID can be used to specify the ringtone of an offline call invitation,
+        //which must be set to the same value as the Push Resource ID in ZEGOCLOUD Admin Console.
+        //This only takes effect when the notifyWhenAppRunningInBackgroundOrQuit is true.
+        newVideoCall.setResourceID("zegouikit_call");
+        newVideoCall.setOnClickListener(v -> {
+            TextInputLayout inputLayout = findViewById(R.id.target_user_id);
+            String targetUserID = inputLayout.getEditText().getText().toString();
+            String[] split = targetUserID.split(",");
+            List<ZegoUIKitUser> users = new ArrayList<>();
+            for (String userID : split) {
+                String userName = userID + "_name";
+                users.add(new ZegoUIKitUser(userID, userName));
+            }
+            newVideoCall.setInvitees(users);
+        });
+    }
+
+    private void initVoiceButton() {
+        ZegoSendCallInvitationButton newVoiceCall = findViewById(R.id.new_voice_call);
+        newVoiceCall.setIsVideoCall(false);
+        //resourceID can be used to specify the ringtone of an offline call invitation,
+        //which must be set to the same value as the Push Resource ID in ZEGOCLOUD Admin Console.
+        //This only takes effect when the notifyWhenAppRunningInBackgroundOrQuit is true.
+        newVoiceCall.setResourceID("zegouikit_call");
+        newVoiceCall.setOnClickListener(v -> {
+            TextInputLayout inputLayout = findViewById(R.id.target_user_id);
+            String targetUserID = inputLayout.getEditText().getText().toString();
+            String[] split = targetUserID.split(",");
+            List<ZegoUIKitUser> users = new ArrayList<>();
+            for (String userID : split) {
+                String userName = userID + "_name";
+                users.add(new ZegoUIKitUser(userID, userName));
+            }
+            newVoiceCall.setInvitees(users);
+        });
+
+        newVoiceCall.setOnClickListener(new ClickListener() {
+            @Override
+            public void onClick(int errorCode, String errorMessage, List<ZegoCallUser> errorInvitees) {
+
+            }
+        });
+    }
+
+    public void initCallInviteService(String generateUserID) {
+        long appID = ;
+        String appSign = "";
+
+        String userID = generateUserID;
+        userName = generateUserID + "_" + Build.MANUFACTURER;
+
+        callInvitationConfig = new ZegoUIKitPrebuiltCallInvitationConfig(
+                ZegoSignalingPlugin.getInstance());
+        callInvitationConfig.notifyWhenAppRunningInBackgroundOrQuit = true;
+        //This property needs to be set when you are building an Android app and when the notifyWhenAppRunningInBackgroundOrQuit is true.
+        //androidNotificationConfig.channelID must be the same as the FCM Channel ID in [ZEGOCLOUD Admin Console|_blank]https://console.zegocloud.com),
+        // and the androidNotificationConfig.channelName can be an arbitrary value.
+        ZegoNotificationConfig notificationConfig = new ZegoNotificationConfig();
+        notificationConfig.sound = "zego_incoming";
+        notificationConfig.channelID = "ZegoUIKit";
+        notificationConfig.channelName = "offlineCall";
+        callInvitationConfig.notificationConfig = notificationConfig;
+        callInvitationConfig.showDeclineButton = getButtonStatus();
+        ZegoUIKitPrebuiltCallInvitationService.init(getApplication(), appID, appSign, userID, userName,
+                callInvitationConfig);
+    }
+
+    private String generateUserID() {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        while (builder.length() < 5) {
+            int nextInt = random.nextInt(10);
+            if (builder.length() == 0 && nextInt == 0) {
+                continue;
+            }
+            builder.append(nextInt);
+        }
+        saveUserId(builder.toString());
+        return builder.toString();
+    }
+
+    private void saveUserId(String userId) {
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putString("userId", userId);
+        edit.apply();
+    }
+
+    private String getUserId() {
+        String spValue = sp.getString("userId", "");
+        if (spValue.isEmpty()) {
+            return generateUserID();
+        } else {
+            return spValue;
+        }
+    }
+
+    private void saveButtonStatus(boolean isShow) {
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putBoolean("isShow", isShow);
+        edit.apply();
+    }
+
+    private boolean getButtonStatus() {
+        boolean spValue = sp.getBoolean("isShow", true);
+        return spValue;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ZegoUIKitPrebuiltCallInvitationService.unInit();
+    }
+
+}
